45a62a48753cc7a7def3bf61f7234310bb772dd1
Michał Kucharski <M.Kucharskov@onet.pl>
2019-06-27 21:53:11 +0200

    Update Not_Art.py

--- Not_Art.py
+++ Not_Art.py
@@ -4,6 +4,25 @@ from PIL import Image
 # Setup variables
 filename = "not_art.png"
 
+# Create array of converted pixels
+def getArray(image):
+    arr = []
+    width, height = image.size
+    rgb = image.convert("RGB")
+    for y in range(0, width, 10):
+        arr.append([])
+        for x in range(0, height, 10):
+            pixel = rgb.getpixel((x,y))
+            arr[y / 10].append(pixel)
+    return arr
+
+# Show 2D array
+def showArray(arr, justSize = 5):
+    for j in range(len(arr)):
+        for i in range(len(arr[j])):
+            print(str(arr[j][i]).rjust(justSize, " "), end="")
+        print("")
+
 # Convert pixel to string value
 def pix2str(pixel):
     str = ""
@@ -15,26 +34,6 @@ def pix2str(pixel):
         elif x == 255:
             str += '2'
     return str
-
-# Create array of converted pixels
-def getArray(image):
-    arr = []
-    width, height = image.size
-    rgb = image.convert("RGB")
-    for y in range(0, width, 10):
-        arr.append([])
-        for x in range(0, height, 10):
-            pixel = rgb.getpixel((x,y))
-            cpix = pix2str(pixel)
-            arr[y / 10].append(cpix)
-    return arr
-
-# Show 2D array
-def showArray(arr, justSize = 5):
-    for j in range(len(arr)):
-        for i in range(len(arr[j])):
-            print(str(arr[j][i]).rjust(justSize, " "), end="")
-        print("")
 
 # Conversion base3 to base10
 def changeBase(num):
@@ -52,12 +51,19 @@ def num2text(num):
 
 # Main function
 def main():
+    # Open image
     image = Image.open(filename)
+    # Get unique grid of pixels
     values = getArray(image)
     for j in range(len(values)):
         for i in range(len(values[j])):
+            # Change RGB into BASE3 string
+            values[j][i] = pix2str(values[j][i])
+            # Chagen BASE3 into BASE10 value
             values[j][i] = changeBase(values[j][i])
+            # Change number into letter (with ROT13)
             values[j][i] = num2text(values[j][i] - 1)
+    # Show an array with 1 char output space
     showArray(values, 1)
 
 if __name__ == '__main__':
